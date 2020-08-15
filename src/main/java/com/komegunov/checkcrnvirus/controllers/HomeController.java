f5a7060532c730fd5ae33b0b5e8642b306da84f6
fishand4ips <alekomegunov@gmail.com>
2020-08-15 13:51:12 +0300

    initial commit

---
+++ src/main/java/com/komegunov/checkcrnvirus/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.komegunov.checkcrnvirus.controllers;
+
+import com.komegunov.checkcrnvirus.models.LocationStats;
+import com.komegunov.checkcrnvirus.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPreviousDay).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
---
+++ src/test/java/com/komegunov/checkcrnvirus/CheckCrnvirusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komegunov.checkcrnvirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCrnvirusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
